ad9356f112eb4ea65fed316d398dde9112054cb6
niuyuxian <ncc0706@gmail.com>
2020-03-16 21:11:36 +0800

    row column

--- lib/demo/demo.dart
+++ lib/demo/demo.dart
@@ -3,10 +3,11 @@ import 'package:flutter_app/demo/post_page.dart';
 import 'package:flutter_app/demo/tab_bar_demo.dart';
 
 import 'helloworld.dart';
+import 'layout_demo.dart';
 
 class Demo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabBarDemo();
+    return LayoutDemo();
   }
 }
